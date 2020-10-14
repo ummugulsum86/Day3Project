03f91b54ed04981b22ee08412766eac4493a9a28
Ummgulsum Koc <umgk86@gmail.com>
2020-10-14 23:53:06 +0200

    added more awesome

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -4,6 +4,10 @@ public class AwesomeTest {
     //green means its under staging area
     //grey in .idea folder means its ignored
 
+    // i just pulled the new change from Github that i did not have locally
+    //and now my project is in sync with remote repository
 
+    //now i made more awesome changes and i am about to push this back to github
 
 }
+
